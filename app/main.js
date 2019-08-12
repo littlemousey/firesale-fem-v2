59e7c28e60dab408fe6f30a0bfefe3b800567a10
Ans de Nijs <ansdenijs@gmail.com>
2019-08-12 15:06:39 +0200

    Initial commit, working readme editor

--- app/main.js
+++ app/main.js
@@ -1,1 +1,102 @@
-console.log('This is an Electron application!');
+const fs = require("fs");
+const { app, BrowserWindow, dialog, Menu } = require("electron");
+
+let mainWindow = null;
+app.on("ready", () => {
+  mainWindow = new BrowserWindow({ show: false });
+
+  Menu.setApplicationMenu(applicationMenu);
+
+  mainWindow.loadFile(`${__dirname}/index.html`);
+
+  mainWindow.once("ready-to-show", () => {
+    mainWindow.show();
+  });
+});
+
+const getFileFromUser = (exports.getFileFromUser = () => {
+  const files = dialog.showOpenDialog(mainWindow, {
+    properties: ["openFile"],
+    buttonLabel: "Unveil",
+    title: "Sesame open document",
+    filters: [
+      {
+        name: "Markdown Files",
+        extensions: ["md", "mdown", "markdown", "marcdown"]
+      },
+      { name: "Text Files", extensions: ["txt", "text"] }
+    ]
+  });
+
+  if (!files) return;
+  const file = files[0];
+  openFile(file);
+});
+
+exports.saveMarkdown = (file, content) => {
+  if (!file) {
+    file = dialog.showSaveDialog(mainWindow, {
+      title: "Save Markdown",
+      defaultPath: app.getPath("desktop"),
+      filters: [
+        {
+          name: "Markdown Files",
+          extensions: ["md", "markdown", "mdown", "marcdown"]
+        }
+      ]
+    });
+  }
+
+  if (!file) return;
+  fs.writeFileSync(file, content);
+  openFile(file);
+};
+
+exports.saveHtml = content => {
+  const file = dialog.showSaveDialog(mainWindow, {
+    title: "Save HTML",
+    defaultPath: app.getPath("desktop"),
+    filters: [{ name: "HTML Files", extensions: ["html", "htm"] }]
+  });
+
+  if (!file) return;
+
+  fs.writeFileSync(file, content);
+};
+
+const openFile = (exports.openFile = file => {
+  const content = fs.readFileSync(file).toString();
+  app.addRecentDocument(file);
+  mainWindow.webContents.send("file-opened", file, content);
+});
+
+const template = [
+  {
+    label: "File",
+    submenu: [
+      {
+        label: "Open File",
+        click() {
+          console.log("Open File");
+        }
+      },
+      {
+        label: "Copy",
+        role: "copy"
+      }
+    ]
+  }
+];
+
+if (process.platform === "darwin") {
+  const applicationName = "Fire Sale";
+  template.unshift({
+    label: applicationName,
+    submenu: [
+      { label: `About ${applicationName}`, role: "about" },
+      { label: `Quit ${applicationName}`, role: "quit" }
+    ]
+  });
+}
+
+const applicationMenu = Menu.buildFromTemplate(template);
